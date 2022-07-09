ea997d407501e9d28a66163ee3f51480efa881f3
tinhcao <caodangtinh@gmail.com>
2022-07-09 10:22:22 +0700

    Initial version of nlebackend generated by generator-jhipster@7.8.1

---
+++ nlebackend/src/test/java/com/nle/TechnicalStructureTest.java
@@ -0,0 +1,37 @@
+package com.nle;
+
+import static com.tngtech.archunit.base.DescribedPredicate.alwaysTrue;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.belongToAnyOf;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packagesOf = NlebackendApp.class, importOptions = DoNotIncludeTests.class)
+class TechnicalStructureTest {
+
+    // prettier-ignore
+    @ArchTest
+    static final ArchRule respectsTechnicalArchitectureLayers = layeredArchitecture()
+        .layer("Config").definedBy("..config..")
+        .layer("Web").definedBy("..web..")
+        .optionalLayer("Service").definedBy("..service..")
+        .layer("Security").definedBy("..security..")
+        .layer("Persistence").definedBy("..repository..")
+        .layer("Domain").definedBy("..domain..")
+
+        .whereLayer("Config").mayNotBeAccessedByAnyLayer()
+        .whereLayer("Web").mayOnlyBeAccessedByLayers("Config")
+        .whereLayer("Service").mayOnlyBeAccessedByLayers("Web", "Config")
+        .whereLayer("Security").mayOnlyBeAccessedByLayers("Config", "Service", "Web")
+        .whereLayer("Persistence").mayOnlyBeAccessedByLayers("Service", "Security", "Web", "Config")
+        .whereLayer("Domain").mayOnlyBeAccessedByLayers("Persistence", "Service", "Security", "Web", "Config")
+
+        .ignoreDependency(belongToAnyOf(NlebackendApp.class), alwaysTrue())
+        .ignoreDependency(alwaysTrue(), belongToAnyOf(
+            com.nle.config.Constants.class,
+            com.nle.config.ApplicationProperties.class
+        ));
+}
