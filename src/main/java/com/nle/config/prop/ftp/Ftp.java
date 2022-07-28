373d577d55f4bd654861f2c85a8c2895b4eee1ff
tinhcao <caodangtinh@gmail.com>
2022-07-28 22:57:32 +0700

    Remove unused properties

--- src/main/java/com/nle/config/prop/ftp/Ftp.java
+++ src/main/java/com/nle/config/prop/ftp/Ftp.java
@@ -5,8 +5,6 @@ import lombok.Data;
 @Data
 public class Ftp {
     private String server;
-    private String username;
-    private String password;
     private String path;
     private String triggerUrl;
     private String triggerToken;
