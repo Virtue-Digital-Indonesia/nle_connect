05090b958b6f753f379cbb1dd7acd9d352da10ef
Daniel-Val <daniel@val.id>
2022-08-24 15:07:31 +0700

    path change to response.redirect

--- src/main/java/com/nle/controller/impersonate/ImpersonateController.java
+++ src/main/java/com/nle/controller/impersonate/ImpersonateController.java
@@ -8,11 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import static com.nle.constant.AppConstant.DEPO_OWNER_IMPERSONATE_TOKEN;
@@ -35,8 +35,9 @@ public class ImpersonateController {
 
     @Operation(description = "Get impersonate token depo owner", operationId = "findByCode", summary = "Get impersonate token depo owner")
     @SecurityRequirement(name = "nleapi")
-    @GetMapping(value = "/impersonate/{token}")
-    public ResponseEntity<JWTToken> forcedIn(@PathVariable("token") String jwtToken) {
+    @GetMapping(value = "/impersonate")
+    public ResponseEntity<JWTToken> forcedIn(HttpServletResponse response) {
+        String jwtToken = response.getHeader("token");
         return ResponseEntity.ok(new JWTToken(jwtToken));
     }
 
--- src/main/java/com/nle/security/impersonate/SwitchUserAuthenticationSuccessHandler.java
+++ src/main/java/com/nle/security/impersonate/SwitchUserAuthenticationSuccessHandler.java
@@ -8,10 +8,7 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
-import static com.nle.constant.AppConstant.DEPO_OWNER_IMPERSONATE_TOKEN;
 
 @Component
 @RequiredArgsConstructor
@@ -21,10 +18,7 @@ public class SwitchUserAuthenticationSuccessHandler extends SavedRequestAwareAut
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         String jwt = tokenProvider.createToken(authentication);
-//        HttpSession session = request.getSession(true);
-//        session.setAttribute(DEPO_OWNER_IMPERSONATE_TOKEN, jwt);
-//        getRedirectStrategy().sendRedirect(request, response, "/api/switchUser");
-//        response.sendRedirect("admin/home.html");
-        getRedirectStrategy().sendRedirect(request, response, "/api/switchUser/impersonate/"+jwt);
+        response.setHeader("token", jwt);
+        response.sendRedirect("/api/switchUser//api/switchUser");
     }
 }
