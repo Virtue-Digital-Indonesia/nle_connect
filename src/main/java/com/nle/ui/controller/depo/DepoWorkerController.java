9387e2a599f7f2f39939c5f6a63eb0f75f6f4105
Daniel-Val <daniel@val.id>
2022-09-09 13:30:55 +0700

    neat path depo worker controller

--- src/main/java/com/nle/ui/controller/depo/DepoWorkerController.java
+++ src/main/java/com/nle/ui/controller/depo/DepoWorkerController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/depo-worker-accounts")
 @RequiredArgsConstructor
 public class DepoWorkerController {
 
@@ -39,7 +39,7 @@ public class DepoWorkerController {
     private final DepoWorkerAccountService depoWorkerAccountService;
 
     @Operation(description = "Depo worker join request", operationId = "depoWorkerJoinRequest", summary = "Depo worker join request")
-    @PostMapping(value = "/depo-worker-accounts/join")
+    @PostMapping(value = "/join")
     public ResponseEntity<ApiResponse> depoWorkerJoinRequest(@RequestBody @Valid DepoWorkerActivationDTO depoWorkerActivationDTO) {
         log.info("Process joining request for depo worker: " + depoWorkerActivationDTO.getFullName());
         depoWorkerAccountService.depoWorkerJoinRequest(depoWorkerActivationDTO);
@@ -47,35 +47,34 @@ public class DepoWorkerController {
     }
 
     @Operation(description = "Complete depo worker registration process", operationId = "completeDepoWorkerRegistration", summary = "Complete depo worker registration process")
-    @PutMapping(value = "/depo-worker-accounts/complete")
+    @PutMapping(value = "/complete")
     public ResponseEntity<DepoWorkerAccountDTO> completeDepoWorkerRegistration(@RequestBody @Valid DepoWorkerUpdateGateNameReqDto depoWorkerUpdateGateNameReqDto) {
         DepoWorkerAccountDTO depoWorkerAccountDTO = depoWorkerAccountService.completeDepoWorkerRegistration(depoWorkerUpdateGateNameReqDto);
         return ResponseEntity.ok(depoWorkerAccountDTO);
     }
 
     @Operation(description = "Check depo worker join request status", operationId = "checkDepoWorkerRegistrationStatus", summary = "Check depo worker join request status")
-    @GetMapping(value = "/depo-worker-accounts/status/{androidId}")
+    @GetMapping(value = "/status/{androidId}")
     public ResponseEntity<ActiveDto> checkDepoWorkerRegistrationStatus(@PathVariable String androidId) {
         AccountStatus accountStatus = depoWorkerAccountService.checkDepoWorkerRegistrationStatus(androidId);
         return ResponseEntity.ok(new ActiveDto(accountStatus.name(), ""));
     }
 
-
     @Operation(description = "Get list of depo worker account with paging", operationId = "getDepoWorkerList", summary = "Get list of depo worker account with paging")
-    @GetMapping(value = "/depo-worker-accounts")
+    @GetMapping
     @SecurityRequirement(name = "nleapi")
     public ResponseEntity<PagingResponseModel<DepoWorkerListDTO>> getDepoWorkerList(Pageable pageable) {
         return ResponseEntity.ok(depoWorkerAccountService.findAll(pageable));
     }
 
     @Operation(description = "Authenticate depo worker account against android id", operationId = "getDepoWorkerList", summary = "Authenticate depo worker account against android id")
-    @PostMapping(value = "/depo-worker-accounts/authenticate")
+    @PostMapping(value = "/authenticate")
     public ResponseEntity<JWTToken> authenticateDepoWorker(@RequestBody @Valid DepoWorkerLoginDto depoWorkerLoginDto) {
         return ResponseEntity.ok(depoWorkerAccountService.authenticateDepoWorker(depoWorkerLoginDto));
     }
 
     @Operation(description = "Get depo worker account details", operationId = "getDepoWorkerAccountDetails", summary = "Get depo worker account details")
-    @GetMapping(value = "/depo-worker-accounts/details")
+    @GetMapping(value = "/details")
     @SecurityRequirement(name = "nleapi")
     public ResponseEntity<DepoWorkerAccountDTO> getDepoWorkerAccountDetails() {
         return ResponseEntity.ok(depoWorkerAccountService.getDepoWorkerAccountDetails());
