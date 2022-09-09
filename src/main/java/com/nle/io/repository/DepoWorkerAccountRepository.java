d371455b65356c5e4a4189ae6b3b4de622b23f66
Daniel-Val <daniel@val.id>
2022-09-09 12:31:08 +0700

    add validation organization code

--- src/main/java/com/nle/io/repository/DepoWorkerAccountRepository.java
+++ src/main/java/com/nle/io/repository/DepoWorkerAccountRepository.java
@@ -1,7 +1,11 @@
 package com.nle.io.repository;
 
 import com.nle.io.entity.DepoWorkerAccount;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +16,8 @@ import java.util.Optional;
 @Repository
 public interface DepoWorkerAccountRepository extends JpaRepository<DepoWorkerAccount, Long> {
     Optional<DepoWorkerAccount> findByAndroidId(String androidId);
+
+    @Query (value = "SELECT * FROM depo_worker_account WHERE organization_code = :code" , nativeQuery = true)
+    Page<DepoWorkerAccount> findAllWorker (@Param("code") String code,
+                                           Pageable pageable);
 }
