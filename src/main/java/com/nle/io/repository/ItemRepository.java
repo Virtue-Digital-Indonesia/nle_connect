1743dc1a7ae04155b4b32e53e9b17d92a7976453
Mohisa Sarwono <mohisa@val.id>
2022-11-08 10:28:35 +0700

    Global Search for Item

--- src/main/java/com/nle/io/repository/ItemRepository.java
+++ src/main/java/com/nle/io/repository/ItemRepository.java
@@ -13,4 +13,31 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query(value = "SELECT it FROM Item it WHERE it.depoOwnerAccount.companyEmail = :companyEmail AND it.deleted = false")
     Page<Item> getAllDepoItem(@Param("companyEmail") String companyEmail, Pageable pageable);
+
+    @Query(value = "SELECT it FROM Item it " +
+            "WHERE it.depoOwnerAccount.companyEmail = :companyEmail " +
+            "AND (:itemName is null OR (lower(it.item_name) like lower(concat('%',:itemName,'%')))) " +
+            "AND (:sku is null OR (lower(it.sku) like lower(concat('%',:sku,'%')))) " +
+            "AND (:description is null OR (lower(it.description ) like lower(concat('%',:description,'%')))) " +
+            "AND (:type is null OR (lower(it.type) like(concat('%',:type,'%')))) " +
+            "AND (:price is null OR (it.price= :price)) " +
+            "AND (:deleted is null OR (it.deleted= :deleted)) " +
+            "AND (:fleetCode is null OR (lower(it.depoFleet.fleet.code) like lower(concat('%',:fleetCode,'%')))) " +
+            "AND (:globalSearch is null " +
+            "       OR (lower(it.item_name) like lower(concat('%',:globalSearch,'%')))" +
+            "       OR (lower(it.sku) like lower(concat('%',:globalSearch,'%'))) " +
+            "       OR (lower(it.description ) like(concat('%',:globalSearch,'%'))) " +
+            "       OR (lower(it.type) like(concat('%',:globalSearch,'%'))) " +
+            "       OR (lower(it.depoFleet.fleet.code) like lower(concat('%',:globalSearch,'%'))) " +
+            ") ")
+    Page<Item> searchItem(String companyEmail,
+                          String itemName,
+                          String sku,
+                          String description,
+                          Integer price,
+                          String type,
+                          Boolean deleted,
+                          String fleetCode,
+                          String globalSearch,
+                          Pageable pageable);
 }
