203ae65b1b9a296f23e58a4b1bb4083906ac600f
Muhamad Ali Nur Zaman <alizaman@val.id>
2022-12-10 08:11:13 +0700

    create api for count all fleet manager

--- src/main/java/com/nle/io/repository/AdminRepository.java
+++ src/main/java/com/nle/io/repository/AdminRepository.java
@@ -1,10 +1,20 @@
 package com.nle.io.repository;
 
 import com.nle.io.entity.admin.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.nle.io.repository.dto.ShippingLineStatistic;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Optional<Admin> findByEmail(String email);
+
+    @Query("select new com.nle.io.repository.dto.ShippingLineStatistic(gm.fleet_manager, count (gm.fleet_manager)) " +
+            "from GateMove gm " +
+            "group by gm.fleet_manager " +
+            "order by count(gm.fleet_manager) desc")
+    List<ShippingLineStatistic> countFleetManager();
 }
