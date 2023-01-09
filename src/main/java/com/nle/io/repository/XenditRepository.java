a10f0cd05d3e4ca0d54e01dc64bf6f869547a515
Muhamad Ali Nur Zaman <alizaman@val.id>
2023-01-09 17:28:23 +0700

    add payment status not expired

--- src/main/java/com/nle/io/repository/XenditRepository.java
+++ src/main/java/com/nle/io/repository/XenditRepository.java
@@ -23,7 +23,7 @@ public interface XenditRepository extends JpaRepository<XenditVA, Long> {
     @Query(value = "SELECT xa FROM XenditVA xa WHERE xa.booking_header_id.id = :booking_id AND xa.payment_status != 'EXPIRED'")
     Optional<XenditVA> findWithBookingID(Long booking_id);
 
-    @Query(value = "SELECT xa FROM XenditVA xa WHERE xa.phone_number = :phone ")
+    @Query(value = "SELECT xa FROM XenditVA xa WHERE xa.phone_number = :phone AND xa.payment_status != 'EXPIRED'")
     List<XenditVA> findWithPhone(String phone);
 
 }
