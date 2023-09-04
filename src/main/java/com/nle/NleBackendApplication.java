cb52d368feb76b66906c9a15384fca3c44b36e9a
Daniel-Val <daniel@val.id>
2023-09-04 09:30:52 +0700

    erase db test connection

--- src/main/java/com/nle/NleBackendApplication.java
+++ src/main/java/com/nle/NleBackendApplication.java
@@ -11,9 +11,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableAsync
@@ -27,15 +24,6 @@ import java.sql.DriverManager;
 public class NleBackendApplication {
 
     public static void main(String[] args) throws Exception {
-        Connection connLocalhost = DriverManager.getConnection(
-                "jdbc:mysql://210.247.245.149:3306/nlebackend?useUnicode=true&characterEncoding=utf8&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true",
-                "nle",
-                ",]O}gtXi|#Y\">");
-
-//        Connection connAPI = DriverManager.getConnection(
-//                "jdbc:mysql://210.247.248.133:3308/nlebackend?enabledTLSProtocols=TLSv1.2", "root", "valid1235");
-
-        System.out.println("Connected?");
         SpringApplication.run(NleBackendApplication.class, args);
     }
 
